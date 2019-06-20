ede712bd7cba2c99dbd7c27330281fb1bbba98ad
BrunoTorresF <bruno.torres.fabila@gmail.com>
2019-06-19 22:09:53 -0500

    edit(content): change project name

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
+    title: `Bruno Torres Fabila`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    author: `Bruno Torres Fabila`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
