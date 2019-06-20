4e4752471073c9e1ba57d836ea22dbb85e2d56c1
BrunoTorresF <bruno.torres.fabila@gmail.com>
2019-06-20 00:21:15 -0500

    edit(layout): changes placeholder img & text

--- src/components/layout.js
+++ src/components/layout.js
@@ -18,7 +18,7 @@ import "./layout.css"
 const Container = styled.div`
   margin: 0 auto;
   max-width: 960px;
-  padding: 0px 1.0875rem 1.45rem;
+  padding: 0px 1.875rem 1.45rem;
   padding-top: 0px;
 `
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -7,14 +7,17 @@ import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <SEO title="Welcome" />
+    <h1>Hi everyone!</h1>
+    <h2>
+      I'm a fullstack software engineer with a penchant for React (and now
+      Gatsby).
+    </h2>
+    <p>This will be the home of my new portfolio site.</p>
+    <p>It's currently under construction so please check back in a few days.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
 
