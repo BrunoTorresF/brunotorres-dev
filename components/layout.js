3c0d512828a96bb0bc081d37e74cf69a5dd481bb
BrunoTorresF <bruno.torres.fabila@gmail.com>
2020-05-12 18:29:55 -0500

    feat(meta): change tab icon

--- components/layout.js
+++ components/layout.js
@@ -9,6 +9,7 @@ export default ({ children, title = 'Bruno Torres | SWE' }) => (
       <title>{title}</title>
       <meta charSet="utf-8" />
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      <link rel="icon" href="/favicon.png"></link>
     </Head>
     <main className={styles.app}>{children}</main>
   </>
