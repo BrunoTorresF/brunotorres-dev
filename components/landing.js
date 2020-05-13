4c59a9c80473144b264bb955fddb0e0d594b702d
BrunoTorresF <bruno.torres.fabila@gmail.com>
2020-05-12 20:36:48 -0500

    feat(icons): add social icons & links

--- components/landing.js
+++ components/landing.js
@@ -1,21 +1,62 @@
 import styles from './landing.module.scss';
-import { FaGithubSquare } from 'react-icons/fa';
+import {
+  FaGithubSquare,
+  FaEnvelope,
+  FaTwitterSquare,
+  FaLinkedin,
+} from 'react-icons/fa';
+import { IconContext } from 'react-icons';
 
 export default () => (
-  <section className={styles.landing}>
-    <div className={styles.landing_content}>
-      <h1 className={`${styles.landing_title} ${styles.white}`}>
-        BRUNO TORRES
-      </h1>
-      <p className={`${styles.landing_subtitle} ${styles.white}`}>
-        I’m a web and software developer
-        <br />
-        that’s passionate about building
-        <br />
-        clean, modern solutions.
-      </p>
-      <div className={styles.social}></div>
-    </div>
-    <div className={styles.landing_img}></div>
-  </section>
+  <IconContext.Provider
+    value={{
+      color: 'rgba(250, 249, 249, 1)',
+      size: '2em',
+      className: 'social_icons',
+    }}>
+    <section className={styles.landing}>
+      <div className={styles.landing_content}>
+        <h1 className={`${styles.landing_title} ${styles.white}`}>
+          BRUNO TORRES
+        </h1>
+        <p className={`${styles.landing_subtitle} ${styles.white}`}>
+          I’m a web and software developer
+          <br />
+          that’s passionate about building
+          <br />
+          clean, modern solutions.
+        </p>
+        <div className={styles.social}>
+          <a
+            href="https://github.com/BrunoTorresF"
+            target="_blank"
+            rel="noopener noreferrer">
+            {' '}
+            <FaGithubSquare />
+          </a>
+          <a
+            href="mailto:contact@brunotorres.dev"
+            target="_blank"
+            rel="noopener noreferrer">
+            <FaEnvelope />
+          </a>
+          <a
+            href="https://twitter.com/BrunoTFMX"
+            target="_blank"
+            rel="noopener noreferrer">
+            {' '}
+            <FaTwitterSquare />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/brunotorresf/"
+            target="_blank"
+            rel="noopener noreferrer">
+            {' '}
+            <FaLinkedin />
+          </a>
+        </div>
+      </div>
+      <div className={styles.landing_img}></div>
+    </section>
+  </IconContext.Provider>
 );
